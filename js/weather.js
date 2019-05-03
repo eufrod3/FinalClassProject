f1c272a0354a4959bf8ad0c244061e94f0455961
eufrod3 <49732139+eufrod3@users.noreply.github.com>
2019-05-02 21:59:19 -0500

    added more js code

--- js/weather.js
+++ js/weather.js
@@ -1,12 +1,15 @@
-$(document).ready(function(){
-    $('#submitWeather').click(function(){
+$(document).ready(function () {
+    $('#submitWeather').click(function () {
         var city = $("#city").val();
 
-        if(city!= ''){
-           $.ajax({
-                url: "https://api.openweathermap.org/data/2.5/weather?APPID=d510695ba94ec51f90c6b9394b5d583e&q=" + city  + "",
-           }); 
-        }else{
+        if (city != '') {
+            $.ajax({
+                url: "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units = imperial" + "&APPID=d510695ba94ec51f90c6b9394b5d583e&",
+                type: "GET",
+                dataType: "jsonp",
+                success: function(data)
+            });
+        } else {
             $("#error").html('field cannot be empty');
         }
     })
