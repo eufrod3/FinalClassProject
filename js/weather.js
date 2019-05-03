1d36734fb1644907863be42cf3719cd13d4644d4
eufrod3 <49732139+eufrod3@users.noreply.github.com>
2019-05-03 15:54:05 -0500

    fixed weather ajax

--- js/weather.js
+++ js/weather.js
@@ -1,16 +1,20 @@
 $(document).ready(function () {
-    $('#submitWeather').click(function () {
-        var city = $("#city").val();
-
-        if (city != '') {
-            $.ajax({
-                url: "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units = imperial" + "&APPID=d510695ba94ec51f90c6b9394b5d583e&",
-                type: "GET",
-                dataType: "jsonp",
-                success: function(data)
-            });
-        } else {
-            $("#error").html('field cannot be empty');
+    $.ajax({
+        url: "https://api.openweathermap.org/data/2.5/weather?q=Dallas&units=imperial&APPID=d510695ba94ec51f90c6b9394b5d583e&",
+        type: "GET",
+        dataType: "jsonp",
+        success: function(data){
+            console.log(data);
+            console.log(data.weather[0].main);
+            console.log(data.main);
+            console.log(data.main.temp);
+            
+            var information = show(data);
+            $("#show").html(information);
         }
-    })
-});
+    });
+})
+
+function show(data){
+    return "<h3>Current Temperature in Dallas: " + Math.round(data.main.temp) + "</h3>"
+}
