b5bed83356a7bcec8a38706e42bad67c64295419
eufrod3 <49732139+eufrod3@users.noreply.github.com>
2019-05-09 23:01:45 -0500

    navbar dropdown

--- js/animate.js
+++ js/animate.js
@@ -1,15 +1,24 @@
 $(document).ready(function(){
-    $("div.weather").mouseenter(function(){
-        $("div.weather").addClass("animated heartBeat slower").one("animationend", function(){
-            $(this).removeClass("animated heartBeat slower");
+    $("div.weather").mouseover(function(){
+        $("div.weather").addClass("animated headShake slower").one("animationend", function(){
+            $(this).removeClass("animated headShake slower");
         });
     });    
 });
 
 $(document).ready(function(){
     $("a.navbar-brand").mouseenter(function(){
-        $("a.navbar-brand").addClass("animated headShake faster").one("animationend", function(){
-            $(this).removeClass("animated headShake faster");
+        $("a.navbar-brand").addClass("animated heartBeat slower").one("animationend", function(){
+            $(this).removeClass("animated heartBeat slower");
         });
     });    
+});
+
+$(document).ready(function(){
+    $("div.weather").mouseenter(function(){
+        $("div.weather").css("background-color", "yellow");
+        });
+    $("div.weather").mouseleave(function(){
+        $("div.weather").css("background-color", "white");
+    });
 });
