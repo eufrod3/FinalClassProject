97ef39b4c66317ae502e4aca33c7b22b9b2ad4e6
eufrod3 <49732139+eufrod3@users.noreply.github.com>
2019-05-09 20:52:21 -0500

    more edits

--- js/animate.js
+++ js/animate.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
-    $("div.weather").mouseover(function(){
-        $("div.weather").addClass("animated heartBeat slow").one("animationend", function(){
-            $(this).removeClass("animated heartBeat slow");
+    $("div.weather").mouseenter(function(){
+        $("div.weather").addClass("animated heartBeat slower").one("animationend", function(){
+            $(this).removeClass("animated heartBeat slower");
         });
     });    
 });
 
 $(document).ready(function(){
-    $("div.jumbotron").mouseenter(function(){
-        $("div.jumbotron").addClass("animated headShake faster").one("animationend", function(){
+    $("a.navbar-brand").mouseenter(function(){
+        $("a.navbar-brand").addClass("animated headShake faster").one("animationend", function(){
             $(this).removeClass("animated headShake faster");
         });
     });    
